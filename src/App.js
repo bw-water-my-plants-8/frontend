bb9b61b002c13d96c91c54d6057a4c7787061902
jMisch44 <jennimisch@gmail.com>
2021-09-20 17:16:06 -0400

    sectioned out App.js by name and moved PlantInfoForm

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ import LoginPage from "./components/LoginPage";
 import icon from "./images/loginIcon.jpg";
 
 function App() {
+  //Jennifer's section
+
+  //Shanae's section
+
+  //John's section
+
   return (
+    //Jennifer's section
+
+    // Shanae's Section
+
     <Route path="/login">
       <LoginPage icon={icon} />
     </Route>
+    // John's section
   );
 }
 
