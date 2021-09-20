a7edb15feff9f51d013c44d6688b6a56c9f50bc0
Shanae <sleslie18@gmail.com>
2021-09-20 16:18:43 -0400

    Login form created

---
+++ src/components/FormStyled.js
@@ -0,0 +1,104 @@
+import styled from "styled-components";
+
+const FormStyled = styled.div`
+  background-color: #ffffff;
+  width: 30%;
+  height: 60%;
+  margin: 7%;
+  margin-top: 11%;
+  border-radius: 5%;
+  box-shadow: 0 0 7px #000;
+  font-family: "Gideon Roman", cursive;
+  h1 {
+    padding-top: 22%;
+    text-align: center;
+    color: #00416a;
+    font-weight: bold;
+    font-size: 2rem;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    padding-top: 40px;
+  }
+  .username {
+    width: 76%;
+    color: rgb(38, 50, 56);
+    font-weight: 700;
+    font-size: 1rem;
+    letter-spacing: 1px;
+    background: rgba(136, 126, 126, 0.04);
+    padding: 10px 20px;
+    border: none;
+    border-radius: 20px;
+    outline: none;
+    box-sizing: border-box;
+    border: 2px solid rgba(0, 0, 0, 0.02);
+    margin-bottom: 50px;
+    margin-left: 46px;
+    text-align: center;
+    margin-bottom: 27px;
+    font-family: "Gideon Roman", cursive;
+  }
+  .password {
+    width: 76%;
+    color: rgb(38, 50, 56);
+    font-weight: 700;
+    font-size: 1rem;
+    letter-spacing: 1px;
+    background: rgba(136, 126, 126, 0.04);
+    padding: 10px 20px;
+    border: none;
+    border-radius: 20px;
+    outline: none;
+    box-sizing: border-box;
+    border: 2px solid rgba(0, 0, 0, 0.02);
+    margin-bottom: 50px;
+    margin-left: 46px;
+    text-align: center;
+    margin-bottom: 27px;
+    font-family: "Gideon Roman", cursive;
+  }
+  .username:focus,
+  .password:focus {
+    border: 2px solid rgba(0, 0, 0, 0.18);
+  }
+  button {
+    margin: auto;
+  }
+  .signIn {
+    cursor: pointer;
+    border-radius: 5em;
+    color: #fff;
+    background: #3ca55c;
+    background: linear-gradient(to left, #00416a, #e4e5e6);
+    border: 0;
+    padding-left: 40px;
+    padding-right: 40px;
+    padding-bottom: 10px;
+    padding-top: 10px;
+    font-family: "Gideon Roman", cursive;
+    font-size: 1rem;
+    box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.04);
+  }
+  .signUp {
+    margin-top: 35px;
+  }
+  a {
+    text-shadow: 0px 0px 3px rgba(117, 117, 117, 0.12);
+    color: #3b8d99;
+
+    text-decoration: none;
+  }
+  .forgot {
+    text-shadow: 0px 0px 3px rgba(117, 117, 117, 0.12);
+  }
+  .warning {
+    color: red;
+    margin: 0;
+    min-height: 1rem;
+  }
+`;
+
+export default FormStyled;
---
+++ src/components/LoginPage.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import LogIconStyled from "./LogIconStyled";
+import schema from "../validation/LoginSchema";
+import FormStyled from "./FormStyled";
+import * as yup from "yup";
+
+const initialFormValues = {
+  username: "",
+  password: "",
+};
+const initialFormErrors = {
+  username: "",
+  password: "",
+};
+const initialDisabled = true;
+
+const LoginPage = (props) => {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
+      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
+  };
+  const inputChange = (evt) => {
+    const { name, value } = evt.target;
+    validate(name, value);
+    setFormValues({ ...setFormValues, [name]: value });
+  };
+  const submit = (evt) => {
+    evt.preventDefault();
+    setFormValues(initialFormValues);
+    setFormErrors(initialFormErrors);
+  };
+
+  return (
+    <>
+      <LogIconStyled>
+        <div>
+          <img src={props.icon} alt="login user icon" />
+        </div>
+      </LogIconStyled>
+
+      <FormStyled>
+        <div>
+          <h1>Login to Your Account</h1>
+          <form onSubmit={submit}>
+            <input
+              className="username"
+              name="username"
+              placeholder="Enter your username"
+              type="text"
+              onChange={inputChange}
+              value={formValues.username}
+            />
+
+            <p className="warning">{formErrors.username}</p>
+
+            <input
+              className="password"
+              name="password"
+              placeholder="Enter your password"
+              type="password"
+              onChange={inputChange}
+              value={formValues.password}
+            />
+
+            <p className="warning">{formErrors.password}</p>
+
+            <button className="signIn">Sign in!</button>
+          </form>
+          <p className="forgot" align="center">
+            <a href="#">Forgot Password?</a>
+          </p>
+          <p align="center" className="signUp">
+            Don't have an account? <a href="/signup">Sign up!</a>
+          </p>
+        </div>
+      </FormStyled>
+    </>
+  );
+};
+
+export default LoginPage;
