107d2320d0c5e3e11239c04a2301cb4034cb5366
jMisch44 <jennimisch@gmail.com>
2021-09-23 17:09:55 -0400

    added delete axios thunk

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,10 +1,5 @@
 export const SET_USER = "SET_USER";
-export const SET_ERROR = "SET_ERROR";
 
 export const setUser = (user) => {
   return { type: SET_USER, payload: user };
-};
-
-export const setError = (error) => {
-  return { type: SET_ERROR, payload: error };
 };
